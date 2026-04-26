6bc475efe3f3c03b2b9ec223eb57308d4bcccefe
Maroshop <homworkhhh25@gmail.com>
2026-04-26 14:52:34 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAFV1QhNHzfetfrfg_C-vFWKmGo8toQ0pI",
+    authDomain: "zorub-ee652.firebaseapp.com",
+    databaseURL: "https://zorub-ee652-default-rtdb.firebaseio.com",
+    projectId: "zorub-ee652",
+    storageBucket: "zorub-ee652.firebasestorage.app",
+    messagingSenderId: "125000693386",
+    appId: "1:125000693386:web:aec04c4734a9ce601a1f73",
+    measurementId: "G-92GM5526WR"
+  },
+
+  firebaseRoot: "cashier_units_pro_zorub_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_zorub_885766842"
+  ]
+};
